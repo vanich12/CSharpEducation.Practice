1502a7f5b024a7fe4ea25ffb8a3844253bdad05f
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-04 17:26:57 +0400

    Task7.Practice2

---
+++ Practice2/Task7/PointC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    internal static class PointC
+    {
+        public static string ParseToRegisterFirstSymb(string str)
+        {
+            string firstSymb = str[0].ToString().ToUpper();
+            string otherWord = new string(str.Skip(1).ToArray());
+            return firstSymb + otherWord;
+        }
+    }
+}
