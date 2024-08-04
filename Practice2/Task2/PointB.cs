fa65d5899c86f72f0fdfd2f745d7e64739f16f9d
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-04 17:25:29 +0400

    Task2.Practice2

---
+++ Practice2/Task2/PointB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal static class PointB
+    {
+        public static double DistanceMacroParse(double value)
+        {
+            return value * 1000 * 100;
+        }
+    }
+}
---
+++ Practice2/Task2/PointD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal static class PointD
+    {
+        public static double TemperatureParse(double value)
+        {
+            return value * 33.8;
+        }
+    }
+}
