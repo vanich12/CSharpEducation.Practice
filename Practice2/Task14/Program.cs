748e4349ddedc60b30dfb55272369bdadc7bab84
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-04 17:29:23 +0400

    Task14.Practice2

---
+++ Practice2/Task14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UtilitiesTest;
+
+namespace ArrayExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            int[] array = CreateEmptyArray(size);
+            FillArray(array);
+            Utils.PrintArray(array);
+        }
+
+        static int[] CreateEmptyArray(int size)
+        {
+            return new int[size];
+        }
+
+        static void FillArray(int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 101);
+            }
+        }
+
+    }
+}
