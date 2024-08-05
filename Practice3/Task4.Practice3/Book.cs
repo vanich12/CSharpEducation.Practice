efb415ba591651da0fa6b3b0774c61ff3ca6bd97
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-05 12:49:09 +0400

    Task4.Practice3

---
+++ Practice3/Task4.Practice3/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Practice3
+{
+    internal class Book
+    {
+        private string Name { get; set; }
+
+        private string Author { get; set; }
+
+        public Book()
+        {
+            this.Name = "Книга Иван";
+            this.Author = "Автор Иван";
+        }
+
+        public Book(string name,string author)
+        {
+            this.Name = name;
+            this.Author = author;
+        }
+    }
+}
