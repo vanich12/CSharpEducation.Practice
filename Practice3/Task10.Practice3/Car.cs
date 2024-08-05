c1697da19dc3730570a7b28eee426261c6f0fb4a
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-05 12:50:13 +0400

    Task10.Practice3

---
+++ Practice3/Task10.Practice3/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10.Practice3
+{
+    internal class Car
+    {
+        private string Mark { get; set; }
+
+        public void GetMark(Car car)
+        {
+            Console.WriteLine($"Название марки этого автомобоиля: {car.Mark}");
+        }
+    }
+}
