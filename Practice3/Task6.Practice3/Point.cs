886ed8462472fc0f82cb4a0424f468c1e4c45d89
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-05 12:49:31 +0400

    Task6.Practice3

---
+++ Practice3/Task6.Practice3/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6.Practice3
+{
+    internal struct Point
+    {
+        private double X { get; set; }
+        private double Y { get; set; }
+
+        public double GetDistance(Point point1, Point point2)
+        {
+            return Math.Sqrt((Math.Pow(point1.X,2) - Math.Pow(point2.X,2))+(Math.Pow(point1.Y,2) - Math.Pow(point2.Y,2)));
+        }
+    }
+}
