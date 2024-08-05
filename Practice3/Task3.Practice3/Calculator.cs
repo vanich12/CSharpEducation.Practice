3883e3612ef2681abfd95f1a306d89294c477a4a
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-05 12:48:56 +0400

    Task3.Practice3

---
+++ Practice3/Task3.Practice3/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Practice3
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль невозможно");
+            }
+            return a / b;
+        }
+    }
+}
