090bdec9100825cfd2d0ff7a464f93fc40601ed1
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-05 12:50:01 +0400

    Task9.Practice3

---
+++ Practice3/Task9.Practice3/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9.Practice3
+{
+    internal class Student
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+
+        public void Rename(Student student)
+        {
+            student.Name = "Аноним";
+        }
+    }
+}
