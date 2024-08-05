26c017a8294826a8bcf5e80d8fb93dba86e77ff2
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-05 12:49:51 +0400

    Task8.Practice3

---
+++ Practice3/Task8.Practice3/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8.Practice3
+{
+    internal struct Triangle
+    {
+        private double Width { get; set; }
+
+        private double Height { get; set; }
+
+        public double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+}
