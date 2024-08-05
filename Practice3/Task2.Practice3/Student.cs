949f8f745b4b95ef47432f9b713cc0fc4d3b01ea
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-05 12:36:12 +0400

    Task2.Practice3

---
+++ Practice3/Task2.Practice3/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Practice3
+{
+    internal class Student
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+
+        private double GPA
+        {
+            get => GPA;
+            set
+            {
+                if (GPA >=0 && GPA <=5)
+                {
+                    GPA = value;
+                }
+            }
+        }
+
+        public Student(string name, int age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+        
+        public void GetFullInfo()
+        {
+            Console.WriteLine($"Имя студента: {Name}");
+            Console.WriteLine($"Возраст студента: {Age}");
+        }
+    }
+}
