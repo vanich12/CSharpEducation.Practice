8662661ef4a3c81129fcc3c0ae46c5f3f345da4f
ivanmirable <ivankrivonogov1823@mail.ru>
2024-08-05 12:50:25 +0400

    Task11.Practice3

---
+++ Practice3/Task11.Practice3/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11.Practice3
+{
+    internal class Book
+    {
+        private string Name { get; set; }
+
+        private string Author { get; set; }
+
+        public string GetIngo(Book book)
+        {
+            return $"Вот название книги: {book.Name},а вот Автор книги: {book.Author} ";
+        }
+    }
+}
